b7847b0e26ede566c9f3c27629231205f5d4b97d
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-29 17:51:45 -0400

    added thread sleeping for better visualization

--- src/cellular_automata_clojure/examples/1d.clj
+++ src/cellular_automata_clojure/examples/1d.clj
@@ -1,18 +1,16 @@
 (ns cellular-automata-clojure.examples.1d
-  (:require [cellular-automata-clojure.core :as core]
-            [cellular-automata-clojure.rules :refer [rule110 rule30]]
-            [cellular-automata-clojure.boards :refer [lonesome-one lonesome-one-short lonesome-one-long]]))
+  (:require [cellular-automata-clojure.core :as core]))
 
 
 (defn do-run [{:keys [generations rule start]
                :as   argmap}]
   (let [start-state (var-get (ns-resolve 'cellular-automata-clojure.boards start)) ruleset (var-get (ns-resolve 'cellular-automata-clojure.rules rule))]
     (loop [i 0 stop-at generations curr-state start-state]
+      (Thread/sleep 50)
       (if (= i stop-at)
         curr-state
         (do (println (core/visualize curr-state)) (flush)
             (recur (inc i) stop-at (core/get-next-state curr-state ruleset)))))))
-
 
 (defn -main 
   ([& args]
