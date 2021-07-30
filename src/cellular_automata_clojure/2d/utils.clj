d4b6f3c06574ae326cfc646045df4351e8bb28eb
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-30 11:36:25 -0400

    passing index to xy tests

--- src/cellular_automata_clojure/2d/utils.clj
+++ src/cellular_automata_clojure/2d/utils.clj
@@ -1,4 +1,28 @@
 (ns cellular-automata-clojure.2d.utils)
 
-(apply vector (repeat 10 (apply vector (repeat 10 1))))
+(defn random-board [] (apply vector (repeatedly 5 #(rand-int 2))))
+(def board (apply vector (repeatedly 5 random-board)))
 
+board
+(flatten board)
+(partition 3 (flatten board))
+(partition 3 3 (partition 3 (flatten board)) (partition 3 3 (flatten board) (flatten board)))
+(apply mapv vector (partition 3 (flatten board)))
+
+
+(defn map-to-xy
+ "takes a flattened index and turns it into an xy position for a square board"
+  [index board]
+  (let [width (count board)]
+    (loop [rem index x 0 y 0]
+      (if (= rem 0)
+        {:x x :y y}
+        (if (> rem width)
+          (recur (- rem width) x (inc y))
+          (recur (dec rem) (inc x) y))))))
+
+(map-to-xy 14 board)
+
+
+(defn get-top-left [index board]
+  )
--- test/cellular_automata_clojure/2d_test.clj
+++ test/cellular_automata_clojure/2d_test.clj
@@ -1,3 +1,15 @@
-(ns cellular-automata-clojure.2d-test)
+(ns cellular-automata-clojure.2d-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [cellular-automata-clojure.2d.utils :as utils]))
 
-
+(deftest index-to-xy-test
+  (let [board (apply vector (repeat 3 (apply vector (repeat 3 0))))]
+    (testing "3x3 board simple"
+      (is (= (utils/map-to-xy 0 board) {:x 0 :y 0})))
+    (testing "3x3 board last digit"
+      (is (= (utils/map-to-xy 8 board) {:x 2 :y 2}))))
+  (let [board (apply vector (repeat 100 (apply vector (repeat 100 0))))]
+    (testing "100x100 board simple"
+      (is (= (utils/map-to-xy 99 board) {:x 99 :y 0})))
+    (testing "100x100 board last digit"
+      (is (= (utils/map-to-xy (dec (* 100 100)) board) {:x 99 :y 99})))))
