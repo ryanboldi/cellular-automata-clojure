6393e0db9b636da732016e1d20baa2fa1cf73301
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-31 09:44:37 -0400

    moved do-run function to utils ns

--- src/cellular_automata_clojure/1d/utils.clj
+++ src/cellular_automata_clojure/1d/utils.clj
@@ -1,4 +1,6 @@
-(ns cellular-automata-clojure.1d.utils)
+(ns cellular-automata-clojure.1d.utils
+  (:require [cellular-automata-clojure.1d.boards]
+            [cellular-automata-clojure.1d.rules]))
 
 (defn apply-rule
   "applies ruleset to length 3 vector (vec)"
@@ -21,3 +23,13 @@
   (->> input
        (map #(if (zero? %) "  " " #"))
        (apply str)))
+
+(defn do-run [{:keys [generations rule start]
+               :as   argmap}]
+  (let [start-state (var-get (ns-resolve 'cellular-automata-clojure.1d.boards start)) ruleset (var-get (ns-resolve 'cellular-automata-clojure.1d.rules rule))]
+    (loop [i 0 stop-at generations curr-state start-state]
+      (Thread/sleep 50)
+      (if (= i stop-at)
+        curr-state
+        (do (println (visualize curr-state)) (flush)
+            (recur (inc i) stop-at (get-next-state curr-state ruleset)))))))
